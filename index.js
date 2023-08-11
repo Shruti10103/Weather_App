fc0e0efaacfbba28b9ad82d6f96609fa72d8945c
Shruti <110061217+Shruti10103@users.noreply.github.com>
2023-08-11 14:42:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const form = document.querySelector("form");
 form.addEventListener("submit", search);
 
 // Default Location
-let target = "delhi";
+let target = "Hyderabad";
 
 // Function to fetch Data from Weather API
 const fetchData = async (target) => {
